078601bee30cb355afe62ada73233678e1d46fdb
Eduardo Amaral <eduardoroamaral@gmail.com>
2019-01-23 16:14:20 -0200

    Added dialog to end game

--- lib/pages/number_sorting_page.dart
+++ lib/pages/number_sorting_page.dart
@@ -55,6 +55,7 @@ class NumberSortingPageState extends State<NumberSortingPage>{
           child: new FloatingActionButton(
               onPressed: () => setState(() {
               print("ALO");
+              _neverSatisfied(context);
             }),
             backgroundColor: Colors.amberAccent,
             tooltip: 'Próximo número',
@@ -64,4 +65,32 @@ class NumberSortingPageState extends State<NumberSortingPage>{
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
+}
+
+Future<void> _neverSatisfied(context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Rewind and remember'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('You will never be satisfied.'),
+              Text('You\’re like me. I’m never satisfied.'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Regret'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
